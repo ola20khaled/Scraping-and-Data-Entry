75dd0edea10d69d21753b49cd3b36858d6b146ba
Ola Khaled <96954973+ola20khaled@users.noreply.github.com>
2022-09-28 06:10:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
+#The web driver for chrome
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get('https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors')
@@ -9,14 +10,17 @@ driver.get('https://www.payscale.com/college-salary-report/majors-that-pay-you-b
 headers = driver.find_elements(By.CLASS_NAME,'data-table__header')
 rows = driver.find_elements(By.CLASS_NAME,'data-table__row')
 
+#Seperate each header of each column
 table_header = []
 for header in headers:
     table_header.append(header.text)
 
+#Seperate each row
 table_rows = []
 for row in rows:
     table_rows.append(row.text)
 
+#Seperate the last 3 columns of each row as they are scrapted as one word
 table_rows1 = []
 for table_row in table_rows:
      row1 = table_row.split("\n")
@@ -30,6 +34,7 @@ for table_row in table_rows:
 
 driver.quit()
 
+#Using pandas to clean the data and save into an excel sheet
 df = pd.DataFrame(columns=table_header, data=table_rows1)
 df.to_excel('data.xlsx')
 
